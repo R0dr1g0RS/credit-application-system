4c65f234f9d78b1ce8c19e37137784b47b96c7f4
R0dr1g0RS <rsantos2004.ce@gmail.com>
2024-02-25 19:09:39 -0300

    Implementation of the Customer Service

--- src/main/kotlin/me/dio/credit/application/system/services/ICustomerService.kt
+++ src/main/kotlin/me/dio/credit/application/system/services/ICustomerService.kt
@@ -5,6 +5,6 @@ import me.dio.credit.application.system.model.Customer
 interface ICustomerService {
     fun save(customer: Customer): Customer
     fun findById(customerId: Long): Customer
-    fun delete(customerId: Long): Customer
+    fun delete(customerId: Long)
 
 }

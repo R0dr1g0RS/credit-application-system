3aed41688e9eb3974bcdeb91550d3b8c9f4c5d16
R0dr1g0RS <rsantos2004.ce@gmail.com>
2024-02-25 20:00:40 -0300

    Implementation of Credit Service

---
+++ src/main/kotlin/me/dio/credit/application/system/services/impl/CreditService.kt
@@ -0,0 +1,36 @@
+package me.dio.credit.application.system.services.impl
+
+import me.dio.credit.application.system.model.Credit
+import me.dio.credit.application.system.repository.CreditRepository
+import me.dio.credit.application.system.repository.CustomerRepository
+import me.dio.credit.application.system.services.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+): ICreditService{
+    override fun save(credit: Credit): Credit{
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> =
+        this.creditRepository.findAllByCustomerId(customerId)
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit =
+            (   this.creditRepository.findByCreditCode(creditCode)
+                ?: throw RuntimeException("Credit code $creditCode not found"))
+
+        return (
+                if (credit.customer?.id == customerId) credit
+                else throw RuntimeException("Contact Admin")
+                )
+    }
+}

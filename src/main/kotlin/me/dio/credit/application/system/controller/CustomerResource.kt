24680936977bd287431feca517853b22e22a9371
R0dr1g0RS <rsantos2004.ce@gmail.com>
2024-02-25 21:34:02 -0300

    Creation of EndPoint [update] of Customer Resource

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -1,15 +1,18 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.controller.dto.CustomerDto
+import me.dio.credit.application.system.controller.dto.CustomerUpdateDto
 import me.dio.credit.application.system.controller.dto.CustomerView
 import me.dio.credit.application.system.model.Customer
 import me.dio.credit.application.system.services.impl.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -33,5 +36,16 @@ class CustomerResource(
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable customerId: Long){
         this.customerService.delete(customerId)
+    }
+
+    @PatchMapping
+    fun update(
+        @RequestParam(value = "customerId") customerId: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView
+    {
+        val customer = this.customerService.findById(customerId)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+        val customerUpdated: Customer = this.customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
     }
 }
